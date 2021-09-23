4560fabe0d5b5a4fef68b253141043aa3ae825e2
frgtnn <rexxdragon11@gmail.com>
2021-09-23 20:54:21 +0200

    Types and Operators plurasight is done

--- Plurasight Beginner/after/js-demo/home.js
+++ Plurasight Beginner/after/js-demo/home.js
@@ -1,3 +1,52 @@
-let price = 25;
+// let price = 12;
+// showMessage(price); /*typeof price, price + 1, price * 3, price / 3, price % 5 (shows the remainder), price += 1 - the same as price = price +1*/ 
+//                     /* taxRate, ++price <- gives us 12+1, 
 
-showMessage(price);
+// let price = 1.1 + 1.3;
+
+// showMessage(price);
+
+// let amount = 0;
+
+// showMessage(amount);
+
+// let message = 'Hello "\World\"'; /* we can also use double quotes, "\World\" to make quotes around words*/
+
+// showMessage(message);
+
+
+// let name = 'Andrea';
+// let message = `Hello 
+
+// ${name}`; /* it give us whitespace between hello and andrea*/
+
+// showMessage(message);
+// console.log(message);
+
+// let message = 'Hello';
+// /*message = message + ' World' gives us "Hello World" -- substring(1) it cuts first letter of 'Hello' word --  message.length (shows how many letters we in word)*/
+// showMessage(message) ;
+
+// let amount = 123;
+// amount = amount.toString();
+// showMessage(typeof amount); it gives us string
+
+// let amount = Number.parseFloat("123.12"); /*it gives as number if we use typeof), if we use A123.12 it will show "NaN", in the end of 123.12 we don't have this reaction*/
+
+// showMessage(typeof amount);
+
+// let saved = true;
+// saved = !saved; /*!saved -- gives us 'false'*/
+// showMessage(saved);
+
+// let saved = 10;
+// saved = null;
+// showMessage(saved);
+// console.log(saved); -- we use this to wipeout our code.
+
+let person = {
+    firstName: 'John',
+    lastName: 'Adams'
+};
+
+console.log(person.firstName);
