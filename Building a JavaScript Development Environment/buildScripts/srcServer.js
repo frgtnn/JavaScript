ee5d925a5a8817ea40577d461e4252b79902020c
frgtnn <rexxdragon11@gmail.com>
2021-10-03 12:37:55 +0200

    Learning about Development Web Server, and how to run int

---
+++ Building a JavaScript Development Environment/buildScripts/srcServer.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../src/index.html'))
+});
+
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
+});
+
+// node buildScripts/srcServer.js to start our server from console.
+// Sharing work-in-progress
+//localtunnel - esasiet setup, ultra-versatile
+//ngrok - easy setup, secure
+//vercel, surge - no firewall hole, hosting persists. Vercel hosts node, surge supports only static files.
+
+
